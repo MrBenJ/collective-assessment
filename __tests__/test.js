fd0a11867bb8ba8de73e2a6747c852ad7400f5e4
Ben Junya <bjunya@gmail.com>
2017-03-15 13:50:58 -0700

    Initial commit

---
+++ __tests__/test.js
@@ -0,0 +1,98 @@
+jest.unmock('../index');
+
+import * as Mod from '../index';
+
+describe('flipArray()', () => {
+    it('Should flip an array in reverse', () => {
+        const arr = [1,2,3,4,5];
+        expect(Mod.flipArray(arr)).toEqual([5,4,3,2,1]);
+    });
+});
+
+describe('getLargest()', () => {
+    it('Should return the largest number in an array', () => {
+        const arr = [1000, 5, 23, 234];
+        expect(Mod.getLargest(arr)).toBe(1000);
+    });
+});
+
+describe('getSmallest()', () => {
+    it('Should return the smallest number in an array', () => {
+        const arr = [1000, 5, 23, 234];
+        expect(Mod.getSmallest(arr)).toBe(5);
+    });
+});
+
+describe('findIndex()', () => {
+    const arr = ['a', 'b', 'f', 'w'];
+
+    it('Should be able to find the index of an element', () => {
+        expect(Mod.findIndex(arr, 'a')).toBe(0);
+    });
+
+    it('Should return -1 if the element does not exist in the array', () => {
+        expect(Mod.findInxed(arr, 'x')).toBe(-1);
+    });
+});
+
+describe('removeDuplicates()', () => {
+    const arr = [1,1,2,3,3,3,4,5,6,6,6,7,9];
+    const strArr = ['a', 'b', 'd', 'd', 'd', 'f', 'a', 'w', 'x'];
+    const boolArr = [true, false, true, false, true, undefined, undefined, null, null];
+    it('Should remove all duplicates', () => {
+        expect(Mod.removeDuplicates(arr)).toEqual(
+            [1,2,3,4,5,6,7,9]
+        );
+    });
+
+    it('Should work with strings and numbers', () => {
+        expect(Mod.removeDuplicates(strArr)).toEqual(
+            ['a', 'b', 'd' ,'f', 'w', 'x']
+        );
+    });
+
+    it('Should work with booleans, null, and undefined', () => {
+        expect(Mod.removeDuplicates(boolArr)).toEqual(
+            [true, false, undefined, null]
+        );
+    });
+});
+
+describe('callOnArray()', () => {
+    const arr = [10,20,30];
+    const double = (item) => { return item * 2};
+
+    it('Should run a function on every item in an array', () => {
+        let answer = Mod.callOnArray(arr, double);
+        expect(answer).toEqual([20,40,60]);
+        expect(Array.isArray(answer)).toBe(true);
+    });
+});
+
+describe('sumOfObjectValues()', () => {
+    const obj = {
+        'hello': 10,
+        'goodbye': 50,
+        'honk': 5,
+        'zing': 20
+    };
+
+    it('Should return sum of an objects\' values', () => {
+        expect(Mod.sumOfObjectValues(obj)).toBe(85);
+    });
+});
+
+describe('Your own tests here', () => {
+
+    describe('capitalize()', () => {
+
+        it('What should be here?', () => {
+
+        });
+
+        it('How about here?', () => {
+
+        });
+
+    });
+});
---
+++ index.js
@@ -0,0 +1,42 @@
+export function flipArray(array) {
+    // Write a function that reverses the order of all the
+    // elements in an array
+}
+
+export function getLargest(array) {
+    // Write a function that returns the largest element
+    // in an array
+}
+
+export function getSmallest(array) {
+    // Write a function that returns the smallest element
+    // in an array
+}
+
+export function findIndex(array, value) {
+    // Write a function takes in an array and a value.
+    // It should return the index of the value
+    // If the value is not found, return -1
+}
+
+export function removeDuplicates(array) {
+    // Write a function that removes all duplicate values in
+    // an array
+}
+
+export function callOnArray(array, callback) {
+    // Write a function that runs `callback` on every member
+    // on `array` and returns an `array` of the returned values
+}
+
+export function sumOfObjectValues(obj) {
+    // Write a function that returns all the values of an object
+    // added together. Assume all values are numbers
+}
+
+export function capitalize(string) {
+    // Write a function that returns a string with the first letter
+    // of the string capitalized.
+    // If a value that isn't a string is passed in, return null
+
+}
